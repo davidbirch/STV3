73c4bde8598a08d6438b707b731de372db1a5cf0
David Birch <birch.dave@gmail.com>
2012-04-30 21:50:19 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+STV3::Application.config.secret_token = '940d009b6ad2eda66d6bb1fdce9385d608acedd3a7756951cf550b09c81221ca1295d118ca41e9feae5eea96922b276c63a4548ba30830c6293ed44ff4c3fa39'
