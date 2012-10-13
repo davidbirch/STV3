c28a3c3d1a0eaba5797fd8297ae0d8ee2023f911
David Birch <birch.dave@gmail.com>
2012-10-13 23:26:36 +1100

    Added images etc, new footer, design changes

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -4,7 +4,7 @@ class RegionsController < ApplicationController
   # GET /regions.json
   def index
     @regions = Region.all
-    
+       
     @title = "Sport on Television in Australia"
     @meta_keywords = "sport, television, tv, coverage, tonight, Australia, Melbourne, Sydney, Brisbane, Adalaide, Perth"
     @meta_description = "Your source for sport on television in Australia.  Find out when sport is on Free-to-air or Pay TV.  Watch live sport on TV tonight."
@@ -21,7 +21,20 @@ class RegionsController < ApplicationController
   # GET /regions/1.json
   def show
     @region = Region.find(params[:id])
-  
+    
+    querystring = "
+    SELECT programs.title, programs.subtitle, programs.description, channels.channel_name, programs.start_datetime, programs.end_datetime
+    FROM programs
+    LEFT JOIN channels 
+    ON programs.channel_id = channels.id
+    WHERE programs.end_datetime >= NOW()
+    AND programs.region_name = '#{params[:id]}'
+    AND programs.sport_name = 'Cricket'
+    ORDER BY programs.channel_xmltv_id
+    "
+       
+    @current_programs = Region.find_by_sql(querystring)
+      
     @title = "Sport on Television in #{@region.region_name}, Australia"
     @meta_keywords = "sport, television, tv, coverage, tonight, Australia, Melbourne, Sydney, Brisbane, Adalaide, Perth"
     @meta_description = "Your source for sport on television in #{@region.region_name}, Australia.  Find out when sport is on Free-to-air or Pay TV.  Watch live sport on TV tonight."
