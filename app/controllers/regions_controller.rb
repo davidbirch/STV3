b13f4710e6bb2bf018e9852ee51a26b2bcc59777
David Birch <birch.dave@gmail.com>
2012-10-19 13:24:10 +1100

    Base CSS finalised

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -3,13 +3,13 @@ class RegionsController < ApplicationController
   # GET /regions
   # GET /regions.json
   def index
-    @regions = Region.all
-       
+
     @title = "Sport on Television in Australia"
     @meta_keywords = "sport, television, tv, coverage, tonight, Australia, Melbourne, Sydney, Brisbane, Adalaide, Perth"
     @meta_description = "Your source for sport on television in Australia.  Find out when sport is on Free-to-air or Pay TV.  Watch live sport on TV tonight."
     @meta_author = "contact@sportontv.com.au"
-    
+    @regions = Region.all
+    @sports = Sport.all
     
     respond_to do |format|
       format.html # index.html.erb
@@ -23,14 +23,16 @@ class RegionsController < ApplicationController
     @region = Region.find(params[:id])
     
     querystring = "
-    SELECT programs.title, programs.subtitle, programs.description, channels.channel_name, programs.start_datetime, programs.end_datetime
+    SELECT DISTINCT programs.title, programs.subtitle, programs.sport_name, channels.channel_name, channels.channel_short_name,
+           DATE_FORMAT(programs.start_datetime,'%a, %d %b %Y') AS start_day, programs.start_datetime,
+           programs.end_datetime, DATE_FORMAT(programs.start_datetime,'%k:%i') AS start_time,
+           DATE_FORMAT(programs.end_datetime,'%k:%i') AS end_time
     FROM programs
     LEFT JOIN channels 
     ON programs.channel_id = channels.id
     WHERE programs.end_datetime >= NOW()
     AND programs.region_name = '#{params[:id]}'
-    AND programs.sport_name = 'Cricket'
-    ORDER BY programs.channel_xmltv_id
+    ORDER BY start_datetime ASC, end_datetime ASC, channel_name ASC, subtitle DESC, title DESC
     "
        
     @current_programs = Region.find_by_sql(querystring)
@@ -39,6 +41,8 @@ class RegionsController < ApplicationController
     @meta_keywords = "sport, television, tv, coverage, tonight, Australia, Melbourne, Sydney, Brisbane, Adalaide, Perth"
     @meta_description = "Your source for sport on television in #{@region.region_name}, Australia.  Find out when sport is on Free-to-air or Pay TV.  Watch live sport on TV tonight."
     @meta_author = "contact@sportontv.com.au"
+    @regions = Region.all
+    @sports = Sport.all
     
     respond_to do |format|
       format.html # show.html.erb
