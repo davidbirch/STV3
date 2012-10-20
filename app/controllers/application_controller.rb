69c7188d932c9ebf4b263587ad1bc5a3c675641b
David Birch <birch.dave@gmail.com>
2012-10-20 15:12:43 +1100

    Refactored layout, CSS, and added login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  #log = Logger.new(File.expand_path("#{Rails.root}/log/controller.log", __FILE__))
+  #log.info("I can log from the application controller")
+  
   protected
     def authenticate
+      basic_auth_yaml = YAML.load_file(File.expand_path("#{Rails.root}/config/basic_auth.yml", __FILE__))
+      
+      #log = Logger.new(File.expand_path("#{Rails.root}/log/controller.log", __FILE__))
+      #log.info("The username is #{basic_auth_yaml["username"]}")
+      
       authenticate_or_request_with_http_basic do |username, password|
-        username == "test" && password == "test"
+        username == basic_auth_yaml["username"] && password == basic_auth_yaml["password"]
       end
     end
     
